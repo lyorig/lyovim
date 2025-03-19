ea32ef3e02297abf2fd2cd49d02c53c496c8f625
lyorig <31075205+lyorig@users.noreply.github.com>
2025-03-19 19:52:21 +0100

    disable showmode

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,9 @@
 -- General stuff
-vim.opt.termguicolors = true -- 24-bit color support
-vim.opt.number = true -- Show line number
+vim.opt.termguicolors = true      -- 24-bit color support
+vim.opt.number = true             -- Show line number
 vim.opt.signcolumn = "yes"
 vim.opt.clipboard = "unnamedplus" -- Sync system clipboard
+vim.opt.showmode = false
 
 -- Tab settings
 vim.opt.shiftwidth = 4
