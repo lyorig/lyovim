f8856fd4f6c2cac8b0d50559259f5c4bb1e55fd5
Petr Šácha <petrik.sacha@gmail.com>
2025-04-12 16:00:12 +0200

    sync + enable java formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -2,10 +2,9 @@ return {
 	"stevearc/conform.nvim",
 
 	opts = {
-		format_on_save = function()
-			if vim.bo.filetype ~= "java" then -- semestrálka
-				return { timeout_ms = 500, lsp_format = "fallback" }
-			end
-		end,
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
 	},
 }
