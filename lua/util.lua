f7d56c8360ea49f788ca1e1f2153bffdbacbd402
Petr Šácha <petrik.sacha@gmail.com>
2025-04-15 19:01:50 +0200

    util.map +remap

--- lua/util.lua
+++ lua/util.lua
@@ -6,6 +6,6 @@ return {
 	---@param action string|function The action to take when `cmd` is inputted.
 	---@param desc string The mapping description, useful for which-key.
 	map = function(mode, cmd, action, desc)
-		vim.keymap.set(mode, cmd, action, { desc = desc })
+		vim.keymap.set(mode, cmd, action, { desc = desc, remap = true })
 	end
 }
