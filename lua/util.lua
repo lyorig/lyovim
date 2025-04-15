f411117d134ce45b046d67cf110eb4ef8ec57129
Petr Šácha <petrik.sacha@gmail.com>
2025-04-15 18:25:04 +0200

    docs

--- lua/util.lua
+++ lua/util.lua
@@ -1,4 +1,10 @@
 return {
+	---Shorthand for `vim.keymap.set` with remapping enabled and a description.
+	---
+	---@param mode string|string[] The mode(s) in which to enable this mapping.
+	---@param cmd string The command which triggers `action`.
+	---@param action string|function The action to take when `cmd` is inputted.
+	---@param desc string The mapping description, useful for which-key.
 	map = function(mode, cmd, action, desc)
 		vim.keymap.set(mode, cmd, action, { desc = desc })
 	end
