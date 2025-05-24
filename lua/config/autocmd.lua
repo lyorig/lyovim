a7797ba1181de76f9eea98638e04d93de0097f28
Petr Šácha <petrik.sacha@gmail.com>
2025-05-24 13:59:54 +0200

    remove nvim-tree autoclose autocmd

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -6,18 +6,3 @@ autocmd("BufWinEnter", {
 		vim.wo.signcolumn = "yes"
 	end,
 })
-
-local function get_win_name(winnr)
-	return vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(winnr))
-end
-
--- Auto-close nvim-tree
-autocmd("BufEnter", {
-	callback = function()
-		local wins = vim.api.nvim_list_wins()
-
-		if #wins == 1 and get_win_name(wins[1]):find("NvimTree_") ~= nil then
-			vim.cmd("qa")
-		end
-	end
-})
