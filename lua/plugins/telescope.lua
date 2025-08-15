1231868b08bb54357fee447dfe98dc5716ca4f94
Petr Šácha <petrik.sacha@gmail.com>
2025-08-16 00:14:13 +0200

    change telescope mappings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,57 +1,57 @@
 return {
-	"nvim-telescope/telescope.nvim",
+    "nvim-telescope/telescope.nvim",
 
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"olacin/telescope-gitmoji.nvim",
-	},
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "olacin/telescope-gitmoji.nvim",
+    },
 
-	config = function()
-		local tele = require "telescope"
-		local tb = require "telescope.builtin"
-		local ta = require "telescope.actions"
+    config = function()
+        local tele = require "telescope"
+        local tb = require "telescope.builtin"
+        local ta = require "telescope.actions"
 
-		tele.setup {
-			defaults = {
-				sorting_strategy = "ascending",
-				initial_mode = "normal",
+        tele.setup {
+            defaults = {
+                sorting_strategy = "ascending",
+                initial_mode = "normal",
 
-				layout_config = {
-					prompt_position = "top",
-				},
+                layout_config = {
+                    prompt_position = "top",
+                },
 
-				mappings = {
-					i = {
-						["<Tab>"] = ta.move_selection_next,
-						["<S-Tab>"] = ta.move_selection_previous,
-					},
+                mappings = {
+                    i = {
+                        ["<Tab>"] = ta.move_selection_next,
+                        ["<S-Tab>"] = ta.move_selection_previous,
+                    },
 
-					n = {
-						["<Tab>"] = ta.move_selection_next,
-						["<S-Tab>"] = ta.move_selection_previous,
-					},
-				},
-			},
+                    n = {
+                        ["<Tab>"] = ta.move_selection_next,
+                        ["<S-Tab>"] = ta.move_selection_previous,
+                    },
+                },
+            },
 
-			extensions = {
-				gitmoji = {
-					initial_mode = "insert",
-				},
-			},
-		}
+            extensions = {
+                gitmoji = {
+                    initial_mode = "insert",
+                },
+            },
+        }
 
-		tele.load_extension("gitmoji")
+        tele.load_extension("gitmoji")
 
-		local map = require "util".map
+        local map = require "util".map
 
-		map("n", "<leader>fg", tb.live_grep, "Telescope: Live grep")
-		map("n", "<leader>ff", tb.find_files, "Telescope: Find files")
-		map("n", "<leader>fr", tb.lsp_references, "Telescope: LSP references")
+        map("n", "<leader>rg", tb.live_grep, "Telescope: ripgrep")
+        map("n", "<leader>ff", tb.find_files, "Telescope: Files")
+        map("n", "<leader>fr", tb.lsp_references, "Telescope: LSP references")
 
-		map("n", "<leader>fc", tb.git_commits, "Telescope: Git commits")
-		map("n", "<leader>fs", tb.git_status, "Telescope: Git status")
+        map("n", "<leader>gc", tb.git_commits, "Telescope: Git commits")
+        map("n", "<leader>gs", tb.git_status, "Telescope: Git status")
 
-		map("n", "<leader>gm", tele.extensions.gitmoji.gitmoji, "Telescope: Gitmoji")
-		map("n", "<leader>gr", tb.resume, "Telescope: Result last search");
-	end
+        map("n", "<leader>gm", tele.extensions.gitmoji.gitmoji, "Telescope: Gitmoji")
+        map("n", "<leader>tr", tb.resume, "Telescope: Result last search");
+    end
 }
